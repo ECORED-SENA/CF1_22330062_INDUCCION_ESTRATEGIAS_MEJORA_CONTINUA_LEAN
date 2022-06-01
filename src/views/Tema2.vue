46f7afdb90067dc9c53c9c4caaebe51a42279ec3
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-06-01 07:55:23 -0500

    Actualizando

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -10,7 +10,7 @@
     figure.mb-4
       img(src='@/assets/curso/tema2/img1.jpg', class='img-fluid')
 
-    .tarjeta.bgr.p-4.p-md-4.mb-5
+    .tarjeta.bgr.p-4.p-md-4
       .row.justify-content-around.align-items-center
         .col-12.col-sm-12.col-md-12.mb-4.mb-md-0
           p Son métodos en los que a partir de la selección de la materia prima y de una variedad de actividades y recursos como la compra de maquinaria, planeación de procesos, atención al cliente, ventas y control de manufactura se articulan para producir un producto. 
@@ -38,6 +38,9 @@
         figure
           img(src='@/assets/curso/tema2/1.png', class='img-fluid')
     br
+    .titulo-sexto.color-acento-contenido.mb-5
+        h5 Figura 4
+        span Sistemas de producción
     .row.mb-5.justify-content-center
       .col-lg-10.mb-5
         .row.justify-content-center(@mouseover="mostrarIndicador = false")
@@ -88,11 +91,52 @@
                 .tarjeta-numerada__contenido
                   p.mb-0 respetar este sistema será producir solo a demanda.
 
+    .row.mb-4 
+      .col-md-5(data-aos="zoom-in")
+        img.p-2(src='@/assets/curso/tema2/vec2.svg' alt='AvatarTop')
 
+    .row.mb-5
+      .col-md-6
+        figure
+          img(src='@/assets/curso/tema2/2.png', class='img-fluid')
+      .col-md-6
+        p Depende de varias características en cuanto a la disponibilidad de la maquinaria dispuesta en los procesos productivos de las fábricas. Asimismo, de varios factores, diversidad de productos, tipos de pedidos, volumen de ventas y frecuencia en los pedidos. 
+        .tarjeta.bgr2.p-2.p-md-3
+          .row.justify-content-around.align-items-center
+            .col-12.col-sm-12.col-md-12.mb-3.mb-md-0
+              p Los sistemas productivos pueden clasificarse según la estructura de los procesos y dependen de la disponibilidad de la maquinaria involucrada en la producción. También de otros factores como variedad de productos, tipos de pedidos, volumen de ventas y frecuencia en la toma de pedidos; de acuerdo con esto se presenta la clasificación del sistema de producción:
+    .titulo-sexto.color-acento-contenido.mb-5
+        h5 Figura 5
+        span Clasificación del sistema de producción
+    .row.mb-5
+      .col-md-12
+        figure
+          img(src='@/assets/curso/tema2/fig.svg')
+    p.mb-4.text-center Nota. Adaptado de Tejeda (2011). https://www.redalyc.org/pdf/870/87019757005.pdf 
+
+    .row.mb-4 
+      .col-md-4(data-aos="zoom-in")
+        img.p-2(src='@/assets/curso/tema2/vec3.svg' alt='AvatarTop')
+
+    .row.mb-5
+      .col-md-6
+        p Según Quiroga (2019) “el proceso productivo es el conjunto de tareas y procedimientos requeridos que realiza una empresa para efectuar la elaboración de bienes y servicios”, dicho de otro modo, es una serie de operaciones y procesos necesarios que se realizan de forma planificada y sucesiva para lograr la elaboración de productos. 
+        .tarjeta.bgr2.p-2.p-md-3
+          .row.justify-content-around.align-items-center
+            .col-12.col-sm-12.col-md-12.mb-3.mb-md-0
+              p Por su parte, las etapas del proceso productivo se describen mediante el flujo de procesos, aplicando una diagramación de procesos para comprender cómo se están desarrollando las actividades utilizando una simbología para cada una de ellas. Esto se puede ver explicado en la siguiente figura:
+      .col-md-6
+        figure
+          img(src='@/assets/curso/tema2/3.png', class='img-fluid')
+      
+    .titulo-sexto.color-acento-contenido.mb-5
+      h5 Figura 6
+      span Simbología para la diagramación del flujo de procesos
+    
     #t_2_2.titulo-segundo.color-acento-contenido.mb-4
-      h2 2.2.	Variables y restricciones inherentes al proceso productivo. 
+      h2 2.2.	Variables y restricciones inherentes al proceso productivo
 
-   
+
           
             
          
