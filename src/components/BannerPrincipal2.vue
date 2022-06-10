2a958f8fb03c1879f2892e67d9113e7f999e4194
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-06-10 10:04:00 -0500

    Actualizado

--- src/components/BannerPrincipal2.vue
+++ src/components/BannerPrincipal2.vue
@@ -1,27 +1,28 @@
 <template lang="pug">
 .banner-principal
-  .container.tarjeta.position-relative(
+
+  .container.tarjeta(
     :style="{'background-image': globalData.fondoBannerPrincipal ? `url(${globalData.fondoBannerPrincipal})` : 'none'}"
   )
-    .row.banner-principal__row.position-relative.justify-content-center.align-items-center
-      .col-lg-7.col-xxl-6.py-5.py-sm-5.banner-principal__info.ps-5
+
+    .row.banner-principal__row
+      .col-lg-7.col-xxl-5.ps-4.ps-sm-5.py-4.py-sm-5.banner-principal__info
         .banner-principal__componente
-          h1.text-white.mb-0(v-html="globalData.componenteFormativo")
-        .col-lg-12
-          .banner-principal__descripcion
-            p.text-white.mb-2(v-html="globalData.descripcionCurso")
+          h1.mb-0(v-html="globalData.componenteFormativo")
+        .banner-principal__descripcion
+          p.mb-0(v-html="globalData.descripcionCurso")
         .banner-principal__accion
           router-link.boton(:to="{name: iniciarLnk.nombreRuta }")
-            span.me-1 Iniciar
+            span.me-1 Ver más
             i.fas.fa-angle-right
 
-      .col-lg-5.d-none.d-lg-block.px-0.position-relative.img-p
+      .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
         img(:src="globalData.imagenBannerPrincipal")
-    .imagen_flotante_1: img(src="@/assets/curso/portada/1.svg").d-none.d-lg-block
-    .imagen_flotante_2: img(src="@/assets/curso/portada/2.svg").d-none.d-lg-block
-    .imagen_flotante_3: img(src="@/assets/curso/portada/3.svg").d-none.d-lg-block
-    .imagen_flotante_4: img(src="@/assets/curso/portada/4.svg").d-none.d-lg-block
-    .imagen_flotante_5: img(src="@/assets/curso/portada/5.svg").d-none.d-lg-block
+        .imagen_flotante_1: img(src="@/assets/curso/portada/1.svg").d-none.d-lg-block
+        .imagen_flotante_2: img(src="@/assets/curso/portada/2.svg").d-none.d-lg-block
+        .imagen_flotante_3: img(src="@/assets/curso/portada/3.svg").d-none.d-lg-block
+        .imagen_flotante_4: img(src="@/assets/curso/portada/4.svg").d-none.d-lg-block
+        .imagen_flotante_5: img(src="@/assets/curso/portada/5.svg").d-none.d-lg-block
  
 
 </template>
@@ -40,45 +41,14 @@ export default {
 </script>
 
 <style lang="sass">
-.img-p
-  top: 0%
-  right: 0%
-  float: right
-  margin: 4em 0 0em 6.8em;
-  @media (max-width:1400px)
-    float: right
-    margin: 4em 0 0em 0em;
-  @media (max-width:1200px)
-    float: right
-    margin: 5em 0 0em 0em;
-
-.fondo-contenido
-  position: absolute
-  padding: 0px
-  z-index: 2
-  height: 100%
-  width: 100%
-.banner-principal__info
-  padding-left: 120px  !important
-  z-index: 3
-  display: flex
-  flex-direction: column
-  justify-content: center
-  @media (max-width: $bp-max-xs)
-    padding: 3em  !important
-    z-index: 3
-    display: flex
-    flex-direction: column
-    justify-content: center
-
 .banner-principal
   p, h1, h2, h3, h4, h5, h6
     color: $color-banner-text
 
   .tarjeta
-    background-color: #FFC664
+    background-color: $color-banner-fondo
     background-size: cover
-    //background-position: center
+    background-position: center
 
   &__info
     display: flex
@@ -93,13 +63,13 @@ export default {
   &__componente
     margin-bottom: 20px
     h1
-      line-height: 1.5em
+      line-height: 1.1em
 
       @media (max-width: $bp-max-xs)
         font-size: 2em
 
   &__descripcion
-    margin-bottom: 30px
+    margin-bottom: 20px
 
   &__row
     @if $banner-principal-img-x == 'derecha'
@@ -111,6 +81,25 @@ export default {
       @media (min-width: $bp-min-sm)
         .banner-principal__img
           padding-right: 3rem!important
+
+  &__img
+    @if $banner-principal-img-y == 'arriba'
+      align-self: flex-start
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-bottom: 3rem!important
+    @else if $banner-principal-img-y == 'abajo'
+      align-self: flex-end
+      padding-top: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+    @else
+      align-self: center
+      padding-top: 1.5rem
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+        padding-bottom: 3rem!important
 
   //&__img
     animation: scale 5s ease-in-out infinite alternate
