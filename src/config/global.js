f2176a87d43c82dab8816db9f4b0688ffe2926f2
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-06-22 20:15:07 -0500

    Actualizado

--- src/config/global.js
+++ src/config/global.js
@@ -391,13 +391,13 @@ export default {
       },
       {
         nombre: 'Pedro Nel Calao Zabala',
-        cargo: 'Validación de contenido',
+        cargo: 'Producción audiovisual',
         centro:
           'Centro Industrial del Diseño y la Manufactura Regional - Santander',
       },
       {
         nombre: 'Yuli Marcela Gómez Tarazona ',
-        cargo: 'Producción audiovisual',
+        cargo: 'Validación de contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura Regional - Santander',
       },
